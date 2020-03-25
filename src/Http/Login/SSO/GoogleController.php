9ad7e5ddca77709cb8e77f77487b34d1a50ef46e
Jonas Weigert <weigert.jonas@gmail.com>
2020-03-25 11:34:07 -0500

    use new Str::random

--- src/Http/Login/SSO/GoogleController.php
+++ src/Http/Login/SSO/GoogleController.php
@@ -11,57 +11,54 @@ use Socialite;
 use Auth;
 use App\User;
 use App\Team;
+use Illuminate\Support\Str;
 
 class GoogleController extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests, AuthenticatesUsers;
-    
+
     public function redirect()
     {
         return Socialite::driver('google')->redirect();
     }
-    
-    public function callback(Request $request) {
-        
+
+    public function callback(Request $request)
+    {
+
         $googleUser = Socialite::driver('google')->user();
         $user = User::where('email', $googleUser->email)->first();
-        
+
         // register user if necessary
-        if(!$user)
-        {
+        if (!$user) {
             $user = new User;
             $user->name = $googleUser->name;
             $user->email = $googleUser->email;
             $user->photo_url = $googleUser->avatar;
-            $user->password = str_random(10);
+            $user->password = Str::random(10);
             $user->save();
         }
-    
+
         // auto-add user to teams based on email domain
-        $emailDomain = substr($user->email, strrpos($user->email, '@')+1, strlen($user->email));
+        $emailDomain = substr($user->email, strrpos($user->email, '@') + 1, strlen($user->email));
         $teams = Team::where('sso_email_domain', $emailDomain)->get();
-        foreach($teams as $team) {
-            
+        foreach ($teams as $team) {
+
             // check if user is on team
-            if(!$user->ownsTeam($team)) {
-                if(!$user->onTeam($team)) {
+            if (!$user->ownsTeam($team)) {
+                if (!$user->onTeam($team)) {
                     // @todo make configurable default role
-                    $user->teams()->sync([$team->id => ['role'=> 'member']]);
+                    $user->teams()->sync([$team->id => ['role' => 'member']]);
                 }
             }
-            
+
             // make sure user has active team on first login
-            if(is_null($user->current_team_id))
-            {
+            if (is_null($user->current_team_id)) {
                 $user->fresh()->switchToTeam($team);
             }
-            
         }
-    
+
         // login user and redirect
         Auth::login($user);
         return $this->sendLoginResponse($request);
-        
     }
-    
 }
